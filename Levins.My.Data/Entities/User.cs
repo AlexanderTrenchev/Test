fcbb27c69bd84f7d41faa1f124611e2d4ddb9417
Аleksandar Тrenchev <aleksandar.trenchev@klevret.com>
2020-01-16 18:50:47 +0200

    Firs Commit

---
+++ Levins.My.Data/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Levins.My.Data.Entities
+{
+    class User : IdentityUser
+    {
+
+    }
+}
---
+++ Levins.My.Data/LevinsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Levins.My.WebApi.Data
+{
+    public class LevinsDbContext : IdentityDbContext
+    {
+        public LevinsDbContext(DbContextOptions<LevinsDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
